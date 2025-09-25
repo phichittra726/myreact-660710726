0597123874868337117c0500d2b8d9b3bfbc4be4
Phichittra Khunnoi <khunnoi_p@su.ac.th>
2025-09-25 03:37:31 +0000

    bookstore101-routing

--- bookstore-app/src/pages/AboutPage.jsx
+++ bookstore-app/src/pages/AboutPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const AboutPage = () => {
     return (
         <div>
-            <h1>Welcome to the  AboutPage</h1>
+            <h1>Welcome to the  AboutPage !!!!!</h1>
         </div>
     )
 } 
